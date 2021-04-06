8d75c35ffe8327f8bddcf484ae7a23f4ff6c108a
Nikita <rusakdeveloepr@gmail.com>
2021-04-06 18:49:08 +0300

    Create auth page

--- src/pages/AuthPage/components/FormLogin.tsx
+++ src/pages/AuthPage/components/FormLogin.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Button from '../../../components/Button';
 import Field from '../../../components/Field';
-import PersonIcon from '@material-ui/icons/Person';
+import MailIcon from '@material-ui/icons/Mail';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import { Link } from 'react-router-dom';
 function FormLogin() {
   return (
-    <form className="form" id="form-auth">
+    <form className="Form-auth" id="form-auth">
+      <h2>Go in</h2>
       <Field
         type="e-mail"
         name="Mail"
         className="Field-primory"
         placeholder="Mail"
         form="form-auth"
-        icon={<PersonIcon />}
+        icon={<MailIcon />}
       />
       <Field
         type="password"
@@ -23,6 +25,9 @@ function FormLogin() {
         icon={<VpnKeyIcon />}
       />
       <Button value="Sign in" className="Button-primary" form="form-auth" type="submit" />
+      <p>
+        <Link to="/registration">Registration</Link> <Link to="/recovery">Recovery</Link>
+      </p>
     </form>
   );
 }
