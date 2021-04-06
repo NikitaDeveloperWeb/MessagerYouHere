85232bdd6d5ab64fb74caa8b1975f320838cf0f2
Nikita <rusakdeveloepr@gmail.com>
2021-04-06 18:51:00 +0300

    Add recovery/register/auth route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
+import RecoveryPage from './pages/RecoveryPage';
+import RegistrationPage from './pages/RegisterPage';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={AuthPage} />
+        <Route exact path="/registration" component={RegistrationPage} />
+        <Route exact path="/recovery" component={RecoveryPage} />
       </Switch>
     </div>
   );
